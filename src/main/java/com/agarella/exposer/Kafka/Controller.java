8907ec454836386beeee1730b86dc48e37ff9794
Ana Arellano <ana.arellano@oracle.com>
2020-03-21 12:18:25 -0600

    Removing kafka and node
    Removing sender and consumer of kafka to enlable service in heroku

--- src/main/java/com/agarella/exposer/Kafka/Controller.java
+++ src/main/java/com/agarella/exposer/Kafka/Controller.java
@@ -1,24 +1,18 @@
 package com.agarella.exposer.Kafka;
 import com.agarella.exposer.Entity.GroupMessage;
 import com.agarella.exposer.Entity.Message;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
-import com.agarella.exposer.Kafka.Sender;
 import com.agarella.exposer.Repository.GroupMessageRepository;
 import com.agarella.exposer.Repository.MessageRepository;
-import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class Controller {
 
-    @Autowired
-    Sender kafkaSender;
     @Autowired
     private GroupMessageRepository repository;
     @Autowired
@@ -27,31 +21,13 @@ public class Controller {
     @RequestMapping(value = "message", method = RequestMethod.POST)
     public ResponseEntity producer(@RequestParam("value") String message,
                                    @RequestParam("user") String user) {
-        kafkaSender.send(message);
         Message msg = Message.builder()
                 .content(message)
                 .user(user)
                 .build();
         msgRepository.save(msg);
-        //Start HTTP Request
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
+        return new ResponseEntity(HttpStatus.CREATED);
 
-        ObjectMapper Obj = new ObjectMapper();
-        try {
-            String jsonStr = Obj.writeValueAsString(msg);
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            String resourceUrl
-                    = "http://localhost:3000/api/kafka/send";//TODO: Change to dynamic URL
-            HttpEntity<String> request = new HttpEntity<String>(jsonStr, headers);
-
-            ResponseEntity<String> response
-                    = restTemplate.postForEntity(resourceUrl,request, String.class);
-            return new ResponseEntity(HttpStatus.CREATED);
-        }catch (IOException e){
-            e.printStackTrace();
-            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
 
     }
     @RequestMapping(value = "messages", method = RequestMethod.GET)
